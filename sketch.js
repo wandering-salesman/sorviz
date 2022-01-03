39a0c25027e95f7eae4745648fe8b4a9b1a01489
wandering-salesman <83429269+wandering-salesman@users.noreply.github.com>
2022-01-03 14:36:25 +0530

    changed canvas size

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 //constants/globals
 var arr = Array(0);
 const width = window.innerWidth;
-const height = window.innerHeight - 200;
+const height = window.innerHeight - 125;
 var thick = 20;
 var len_arr = Math.floor(width / thick);
 var sorted_arr = Array(0);
 var start_sorting = false;
-var frame_rate_val = 40;
+var frame_rate_val = 60;
 var timer_arr = [];
 var time_taken = 0;
 var pause = false;
